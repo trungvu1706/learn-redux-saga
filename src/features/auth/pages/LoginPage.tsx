88919a8bec4c204282f5226f40b669e2b4b73326
TrungVu <trungvu1706@gmail.com>
2021-09-13 23:28:32 +0700

    code UI for login page

--- src/features/auth/pages/LoginPage.tsx
+++ src/features/auth/pages/LoginPage.tsx
@@ -1,9 +1,37 @@
+import { Box, Button, makeStyles, Paper, Typography } from '@material-ui/core';
 import React from 'react';
 
-interface Props {}
+const useStyle = makeStyles((theme) => ({
+  root: {
+    display: 'flex',
+    justifyContent: 'center',
+    flexFlow: 'row nowrap',
+    alignItems: 'center',
+    minHeight: '100vh',
+  },
 
-const LoginPage = (props: Props) => {
-  return <div>Login Page</div>;
+  box: {
+    padding: theme.spacing(3),
+  },
+}));
+
+const LoginPage = () => {
+  const classes = useStyle();
+  return (
+    <Box className={classes.root}>
+      <Paper elevation={3} className={classes.box}>
+        <Typography variant="h5" component="h1">
+          Student Management
+        </Typography>
+
+        <Box mt={4}>
+          <Button variant="contained" color="primary" fullWidth>
+            Fake login
+          </Button>
+        </Box>
+      </Paper>
+    </Box>
+  );
 };
 
 export default LoginPage;
--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,18 @@
+import { CssBaseline } from '@material-ui/core';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import { store } from './app/store';
 import { Provider } from 'react-redux';
 import { BrowserRouter as Router } from 'react-router-dom';
+import App from './App';
+import { store } from './app/store';
+import './index.css';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
       <Router>
+        <CssBaseline />
         <App />
       </Router>
     </Provider>
